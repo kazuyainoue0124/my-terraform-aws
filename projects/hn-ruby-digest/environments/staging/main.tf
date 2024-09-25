e8d0209c46dd49ee3f2c2c7cd6dc3be04dac8ca8
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-25 21:07:11 +0900

    モジュール化の前に実行できることを確認

--- projects/hn-ruby-digest/environments/staging/main.tf
+++ projects/hn-ruby-digest/environments/staging/main.tf
@@ -10,3 +10,46 @@ module "registered_emails" {
     Project     = "HN Ruby Digest"
   }
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "../../applications/lambda_function.rb"
+  output_path = "../../applications/lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "hn_ruby_digest_staging" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "../../applications/lambda_function_payload.zip"
+  function_name = "hn_ruby_digest_staging"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "ruby3.3"
+
+  environment {
+    variables = {
+      ENV = "staging"
+    }
+  }
+}
